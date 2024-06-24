c0be1ee2785c5272ae5cdc1fb8dfd3d6dcd500ed
missosmal <fadesteam8@gmail.com>
2024-06-24 22:24:21 +0500

    Обработка нажатия.Graf

--- app/src/main/java/com/example/pr11_graf_01_03/MainActivity.java
+++ app/src/main/java/com/example/pr11_graf_01_03/MainActivity.java
@@ -2,23 +2,42 @@ package com.example.pr11_graf_01_03;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
+    public void AlertDialog(String title, String message)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle(title)
+                .setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+    public void OnClick(View view){
+        AlertDialog("Уведомление", "Нажатие обработано");
+    }
+
 }
